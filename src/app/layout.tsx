15a074b8d0f9f9a65d85972f374360dc96bdcc46
Thomas Jacob <jeevanjj004@gmail.com>
2026-05-28 09:04:53 +0530

    sitemap is added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -113,6 +113,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+      <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
